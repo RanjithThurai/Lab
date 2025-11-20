3c03fe6a3f2d899e08e9929dbebbd9d7919fb4d5
RanjithThurai <ranjithranjith9210@gmail.com>
2025-11-20 11:06:33 +0530

    2nd commit

--- test_app.py
+++ test_app.py
@@ -4,11 +4,11 @@ from app import add, subtract, multiply
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(0, 1) == 1
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
-    assert subtract(-1, 4) == -3
+    assert subtract(-1, -1) == 0
 def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(-1, 1) == -1
